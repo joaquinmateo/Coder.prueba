f0fbd073e24a809f95a7ca7a17a8d4e5d43e0225
Joaquin Mateo <joaquinmateo2003@gmail.com>
2024-09-03 17:03:44 -0300

    Rama modificada

--- personas.py
+++ personas.py
@@ -1,7 +1,8 @@
 class Persona():
-    def __init__(self, nombre, apellido):
+    def __init__(self, nombre, apellido, edad):
         self.nombre = nombre
         self.apellido = apellido
+        self.edad = edad
 
     def saluda(self):
         return f'Hola soy {self.nombre}'
