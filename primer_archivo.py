99ec3f462fac277958bd83a42d80bfdcef7db576
Joaquin Mateo <joaquinmateo2003@gmail.com>
2024-09-03 16:33:29 -0300

    mensaje de despedida

--- primer_archivo.py
+++ primer_archivo.py
@@ -1,3 +1,9 @@
-#Clic derecho en carptea vacía, abrir con git bash, en la terminal escribir "init + (nombre de la carpeta)", esa carpeta la abrimos con VS Code
+#Clic derecho en carptea vacía, abrir con git bash, en la terminal escribir "init + (nombre de la carpeta)"
+#Esa carpeta la abrimos con VS Code y le creamos un archivo
+#En la terminal escribimos git add (nombre del archivo)
+#Si escribimos git commit -m "(el mensaje que queramos para reconocer)" hace un checkpoint
+#Si nosotros seguimos escribiendo código sin el commit no se guardará en el git aunque lo guardemos en vscode
 print('Hola Mundo')
+
+print('Adios Mundo Cruel')
 
