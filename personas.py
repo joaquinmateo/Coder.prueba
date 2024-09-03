24e061619f42ec369be23603ae2cd07cc0616444
Joaquin Mateo <joaquinmateo2003@gmail.com>
2024-09-03 16:37:46 -0300

    agrego saludo

--- personas.py
+++ personas.py
@@ -2,3 +2,6 @@ class Persona():
     def __init__(self, nombre, apellido):
         self.nombre = nombre
         self.apellido = apellido
+
+    def saluda(self):
+        return f'Hola soy {self.nombre}'
