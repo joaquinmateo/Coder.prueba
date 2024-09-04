c15783bd98e7c8ef9a894b13555e45292fa57350
Joaquin Mateo <joaquinmateo2003@gmail.com>
2024-09-04 16:24:16 -0300

    tomando apuntes

--- primer_archivo.py
+++ primer_archivo.py
@@ -3,6 +3,12 @@
 #En la terminal escribimos git add (nombre del archivo)
 #Si escribimos git commit -m "(el mensaje que queramos para reconocer)" hace un checkpoint
 #Si nosotros seguimos escribiendo código sin el commit no se guardará en el git aunque lo guardemos en vscode
+#git log --oneline , para tener el historial de cambios, esto varía si estoy en una rama u otra
+#git branch -m (nomrbre de la rama)
+#git checkout (nombre de la rama)
+#git branch -l , de ésta manera vemos las ramas existentes
+#git branch -D (nombre de la rama) , para borrar la rama
+#
 print('Hola Mundo')
 
 print('Adios Mundo Cruel')
